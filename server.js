7e083d6d72b3677056b49e7ff522c2c1fd5edfce
Aniket <suryawanshianiket7576@gmail.com>
2026-06-05 18:20:05 +0530

    Update

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-dotenv.config(); 
 import dotenv from "dotenv";
-         // ✅ Sabse pehle
-
+dotenv.config();
+      
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -51,6 +50,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
+  "http://localhost:3000",
   "https://vedic-murex.vercel.app"
 ];
 
